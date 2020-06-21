d4c508d98707a0371caf80304b442caef4b0121b
刘嘉琦 <136112871@qq.com>
2020-06-21 21:18:17 +0800

    完成上传下载文件

--- njustShare2/src/main/java/com/njustshare/demo/controller/FileController.java
+++ njustShare2/src/main/java/com/njustshare/demo/controller/FileController.java
@@ -3,19 +3,20 @@ package com.njustshare.demo.controller;
 import com.njustshare.demo.entity.FileInfo;
 import com.njustshare.demo.service.FileService;
 import org.apache.ibatis.annotations.Param;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 
 @RestController
 @RequestMapping("/file")
 public class FileController {
+    Logger logger = LoggerFactory.getLogger(FileController.class);
 
     @Autowired
     FileService fileService;
@@ -31,5 +32,24 @@ public class FileController {
         list = fileService.getFileList(college,major,grade);
         map.put("result",list);
         return map;
+    }
+
+    @PostMapping("/uploads")
+    @ResponseBody
+    public String upload(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return "上传失败，请选择文件";
+        }
+        try {
+            String fileName = file.getOriginalFilename();
+            String filePath = ResourceUtils.getURL("classpath:").getPath()+"/document/";
+            File dest = new File(filePath + fileName);
+            file.transferTo(dest);
+            logger.info("上传成功");
+            return "上传成功";
+        } catch (IOException e) {
+            logger.error(e.toString(), e);
+        }
+        return "上传失败！";
     }
 }
